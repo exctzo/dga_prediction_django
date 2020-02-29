1e3de4f120db98c8c3e46995158fa3937c0e0213
Denis Galiakhmetov <exctzo@gmail.com>
2020-02-29 22:13:21 +0300

    untitled

--- sniff/views.py
+++ sniff/views.py
@@ -1,5 +1,6 @@
 import json
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import TemplateView
 from .tasks import *
