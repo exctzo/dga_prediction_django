cb00fa1390c9cf4a2b97328cf00ea9325fd6fd4c
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-06 22:05:26 +0300

    tmp

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .views import profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UsersTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='test_user', password='test_pass', email='test@mail.ru', local_dns_ip='101.10.10.101')
+
+    def test_user_profile(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/profile')
+
+        # Simulate a logged-in user by setting request.user manually.
+        request.user = self.user
+
+        # Test profile() view
+        response = profile(request)
+
+        # Get correct status code
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_exist(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/profile')
+
+        user = User.objects.get(username='admin')
+
+        # Simulate a logged-in user by setting request.user manually.
+        request.user = self.user
+
+        # Test profile() view
+        response = profile(request)
+
+        # Get correct status code
+        self.assertEqual(response.status_code, 200)
--- sniff/tests.py
+++ sniff/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
+from .models import Requests
 
-# Create your tests here.
+
+class SniffTestCase(TestCase):
+    def setUp(self):
+        # self.factory = RequestFactory()
+        self.req = Requests.objects.create(ip_dst='', ip_src='243.19.14.144', qname='ajsfhkjshf.com', dga='1', dga_proba='0.7', dga_subtype='fwefsd', dga_subtype_proba = '0.3')
+
+    def test_requests_models(self):
+        req = Requests.objects.get(ip_src='243.19.14.144')
+        self.assertEqual(req.qname,'ajsfhkjshf.com')
