c7ae0cc4ab0b14cc6626c33e01755d3bbab2aefe
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-06 22:33:04 +0300

    tmp

--- sniff/tests.py
+++ sniff/tests.py
@@ -5,7 +5,7 @@ from .models import Requests
 class SniffTestCase(TestCase):
     def setUp(self):
         # self.factory = RequestFactory()
-        self.req = Requests.objects.create(ip_dst='', ip_src='243.19.14.144', qname='ajsfhkjshf.com', dga='1', dga_proba='0.7', dga_subtype='fwefsd', dga_subtype_proba = '0.3')
+        self.req = Requests.objects.create(ip_dst='68.183.21.239', ip_src='243.19.14.144', qname='ajsfhkjshf.com', dga='1', dga_proba='0.7', dga_subtype='fwefsd', dga_subtype_proba = '0.3')
 
     def test_requests_models(self):
         req = Requests.objects.get(ip_src='243.19.14.144')
