101338dd95179d24fcdc62e16294b8f99984a0ea
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-21 11:04:43 +0300

    untitled

--- home/models.py
+++ home/models.py
@@ -2,6 +2,9 @@ from django.db import models
 # from django.contrib.auth import get_user_model
 
 class User(models.Model) :
+
+	REQUIRED_FIELDS = ('username','password','email','local_dns_ip')
+
 	username = models.CharField(max_length=250, primary_key=True)
 	password = models.CharField(max_length=50)
 	email = models.EmailField(max_length=250, unique=True)
