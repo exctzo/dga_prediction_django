19c22ef6a3a74c204bcc83a2e721dee46de2c85d
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-21 11:18:31 +0300

    untitled

--- home/models.py
+++ home/models.py
@@ -11,8 +11,8 @@ class User(AbstractBaseUser) :
 	password = models.CharField(max_length=50)
 	email = models.EmailField(max_length=250, unique=True)
 	local_dns_ip = models.CharField(max_length=30, default = '0.0.0.0')
-
-    objects = UserManager()
+	
+	objects = UserManager()
 
 # user_model = get_user_model()
 # user_model.add_to_class("local_dns_ip", models.GenericIPAddressField(default = '0.0.0.0'))
