32a9acaf89200d7109c66345a9ad1d1b23d33ff3
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-21 11:37:28 +0300

    untitled

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractBaseUser, UserManager, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
 # from django.contrib.auth import get_user_model
 
-class User(AbstractBaseUser,BaseUserManager) :
+class User(AbstractBaseUser) :
 
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = ('password','email','local_dns_ip')
@@ -11,8 +11,14 @@ class User(AbstractBaseUser,BaseUserManager) :
 	password = models.CharField(max_length=50)
 	email = models.EmailField(max_length=250, unique=True)
 	local_dns_ip = models.CharField(max_length=30, default = '0.0.0.0')
-	
-	objects = UserManager()
+    is_superuser = models.BooleanField(default=False) 
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+	# objects = UserManager()
 
 # user_model = get_user_model()
 # user_model.add_to_class("local_dns_ip", models.GenericIPAddressField(default = '0.0.0.0'))
