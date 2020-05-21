6f3e4de7789cf6ce93a239379b597d6cf3d19bc1
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-21 11:11:19 +0300

    untitled

--- home/models.py
+++ home/models.py
@@ -1,14 +1,14 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 # from django.contrib.auth import get_user_model
 
-class User(models.Model) :
+class User(AbstractBaseUser) :
 
-	USERNAME_FIELD = 'username'
-	REQUIRED_FIELDS = ('username','password','email','local_dns_ip')
+	# USERNAME_FIELD = 'username'
+	# REQUIRED_FIELDS = ('username','password','email','local_dns_ip')
 
-	username = models.CharField(max_length=250, primary_key=True)
-	password = models.CharField(max_length=50)
-	email = models.EmailField(max_length=250, unique=True)
+	# username = models.CharField(max_length=250, primary_key=True)
+	# password = models.CharField(max_length=50)
+	# email = models.EmailField(max_length=250, unique=True)
 	local_dns_ip = models.CharField(max_length=30, default = '0.0.0.0')
 
 # user_model = get_user_model()
