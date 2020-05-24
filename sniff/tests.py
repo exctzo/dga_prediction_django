3ef73b955db3e89719dda32f6a3188183f22a63e
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-24 05:29:56 +0300

    untitled

--- sniff/tests.py
+++ sniff/tests.py
@@ -7,6 +7,33 @@ from scapy.all import *
 from scapy.layers.dns import DNS
 from scapy.layers.inet import IP
 
+def SendTCP(iv_dns_up_ip, iv_query):
+    # Отправка tcp запросов на вышестоящий dns сервер.
+    lv_server = (iv_dns_up_ip, 53)
+    lv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lv_sock.connect(lv_server)
+
+    # Перестройка tcp запроса на основе udp.
+    lv_pay = chr(len(iv_query))
+    lv_tcp_query = b'\x00' + lv_pay.encode() + iv_query
+
+    lv_sock.send(lv_tcp_query)  	
+    lv_data = lv_sock.recv(1024)
+    return lv_data
+
+
+def Handler(iv_data, iv_addr, iv_socket, iv_dns_up_ip, iv_interface):
+    # Новый поток для обработки udp запроса на tcp запрос.
+    lv_TCPanswer = SendTCP(iv_dns_up_ip, iv_data)
+    lv_UDPanswer = lv_TCPanswer[2:]
+    iv_socket.sendto(lv_UDPanswer, iv_addr)
+
+    # Извлечение запрашиваемого домена, ip адресов.
+    lv_ip_src = iv_addr[0]
+    lv_ip_dst = gv_interface_ip
+    lv_layerDNS = DNS(iv_data)
+    lv_qname = lv_layerDNS.qd.qname.decode("utf-8")
+
 class SniffTestCase(TestCase):
     def setUp(self):
         self.req = Request.objects.create(ip_dst='68.183.21.239', ip_src='243.19.14.144', 
@@ -22,33 +49,6 @@ class SniffTestCase(TestCase):
 
         # Проверка корректности полученного доменного имени
         self.assertEqual(req.qname,'ajsfhkjshf.com')
-
-    def SendTCP(iv_dns_up_ip, iv_query):
-        # Отправка tcp запросов на вышестоящий dns сервер.
-        lv_server = (iv_dns_up_ip, 53)
-        lv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        lv_sock.connect(lv_server)
-
-        # Перестройка tcp запроса на основе udp.
-        lv_pay = chr(len(iv_query))
-        lv_tcp_query = b'\x00' + lv_pay.encode() + iv_query
-
-        lv_sock.send(lv_tcp_query)  	
-        lv_data = lv_sock.recv(1024)
-        return lv_data
-
-
-    def Handler(iv_data, iv_addr, iv_socket, iv_dns_up_ip, iv_interface):
-        # Новый поток для обработки udp запроса на tcp запрос.
-        lv_TCPanswer = sendtcp(iv_dns_up_ip, iv_data)
-        lv_UDPanswer = lv_TCPanswer[2:]
-        iv_socket.sendto(lv_UDPanswer, iv_addr)
-
-        # Извлечение запрашиваемого домена, ip адресов.
-        lv_ip_src = iv_addr[0]
-        lv_ip_dst = gv_interface_ip
-        lv_layerDNS = DNS(iv_data)
-        lv_qname = lv_layerDNS.qd.qname.decode("utf-8")
     
     def test_proxy_requests(self):
         iv_dns_up_ip = '8.8.8.8'
@@ -60,7 +60,7 @@ class SniffTestCase(TestCase):
         lv_sock.bind((lv_host, iv_port))
 
         lv_data, lv_addr = lv_sock.recvfrom(1024)
-        lv_th = Thread(target=handler, args=(lv_data, lv_addr, lv_sock, iv_dns_up_ip, iv_interface))
+        lv_th = Thread(target=Handler, args=(lv_data, lv_addr, lv_sock, iv_dns_up_ip, iv_interface))
         lv_th.start()
         
         resolver = pydig.Resolver(executable='/usr/bin/dig', nameservers=['exctzo.tech',], additional_args=['-p 9981',])
