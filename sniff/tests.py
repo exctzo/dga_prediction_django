6343a06926cc7dccdfb2189432168d6b5d9cb005
Denis Galiakhmetov <exctzo@gmail.com>
2020-05-24 04:48:57 +0300

    untitled

--- sniff/tests.py
+++ sniff/tests.py
@@ -1,5 +1,6 @@
 from django.test import RequestFactory, TestCase
 from .models import Request
+import socket
 
 class SniffTestCase(TestCase):
     def setUp(self):
@@ -13,3 +14,44 @@ class SniffTestCase(TestCase):
 
         # Проверка корректности полученного доменного имени
         self.assertEqual(req.qname,'ajsfhkjshf.com')
+
+    def SendTCP(iv_dns_up_ip, iv_query):
+        # Отправка tcp запросов на вышестоящий dns сервер.
+        lv_server = (iv_dns_up_ip, 53)
+        lv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lv_sock.connect(lv_server)
+
+        # Перестройка tcp запроса на основе udp.
+        lv_pay = chr(len(iv_query))
+        lv_tcp_query = b'\x00' + lv_pay.encode() + iv_query
+
+        lv_sock.send(lv_tcp_query)  	
+        lv_data = lv_sock.recv(1024)
+        return lv_data
+
+
+    def Handler(iv_data, iv_addr, iv_socket, iv_dns_up_ip, iv_interface):
+        # Новый поток для обработки udp запроса на tcp запрос.
+        lv_TCPanswer = SendTCP(iv_dns_up_ip, iv_data)
+        lv_UDPanswer = lv_TCPanswer[2:]
+        iv_socket.sendto(lv_UDPanswer, iv_addr)
+
+        # Извлечение запрашиваемого домена, ip адресов.
+        lv_ip_src = iv_addr[0]
+        lv_ip_dst = gv_interface_ip
+        lv_layerDNS = DNS(iv_data)
+        lv_qname = lv_layerDNS.qd.qname.decode("utf-8")
+    
+    def test_proxy_requests(self):
+        iv_dns_up_ip = '8.8.8.8'
+        iv_interface = os.listdir('/sys/class/net/')[1]
+        iv_port = '9981'
+        lv_host = ''
+        # Настройка udp сервера для получения dns запросов.
+        lv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        lv_sock.bind((lv_host, iv_port))
+
+        lv_data, lv_addr = lv_sock.recvfrom(1024)
+        lv_th = Thread(target=Handler, args=(lv_data, lv_addr, lv_sock, iv_dns_up_ip, iv_interface))
+        lv_th.start()
+
